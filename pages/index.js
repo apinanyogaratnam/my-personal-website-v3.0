23713894230505a9184da23432c34472547b2156
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-12-27 08:51:17 -0500

    created navbar component

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,16 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import Navbar from '././components/navbar';
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Apinan Yogaratnam</title>
-        <meta name="description" content="Apinan Yogaratnam's Personal Website" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-    </div>
-  )
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Apinan Yogaratnam</title>
+                <meta name="description" content="Apinan Yogaratnam's Personal Website" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+        </div>
+    );
 }
