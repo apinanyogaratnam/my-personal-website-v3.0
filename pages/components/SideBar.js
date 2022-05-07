179f03f686e9be763e70fbdbb1be9476717b9e54
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2022-05-06 23:18:39 -0400

    chore: reinstall dependencies

---
+++ pages/components/SideBar.js
@@ -0,0 +1,31 @@
+import {
+    List,
+    ListItem,
+    ListIcon,
+    OrderedList,
+    UnorderedList,
+  } from '@chakra-ui/react'
+
+export default function SideBar() {
+    return (
+        <List spacing={3}>
+        <ListItem>
+            <ListIcon as={MdCheckCircle} color='green.500' />
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit
+        </ListItem>
+        <ListItem>
+            <ListIcon as={MdCheckCircle} color='green.500' />
+            Assumenda, quia temporibus eveniet a libero incidunt suscipit
+        </ListItem>
+        <ListItem>
+            <ListIcon as={MdCheckCircle} color='green.500' />
+            Quidem, ipsam illum quis sed voluptatum quae eum fugit earum
+        </ListItem>
+        {/* You can also use custom icons from react-icons */}
+        <ListItem>
+            <ListIcon as={MdSettings} color='green.500' />
+            Quidem, ipsam illum quis sed voluptatum quae eum fugit earum
+        </ListItem>
+        </List>
+    )
+}
