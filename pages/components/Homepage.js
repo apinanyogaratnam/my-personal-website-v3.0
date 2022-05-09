5f9be28fd2d9e5747ba1d564e580446d34e76433
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2022-05-08 23:42:19 -0400

    refactor: created homepage module

--- pages/components/Homepage.js
+++ pages/components/Homepage.js
@@ -1,9 +1,31 @@
+import Image from 'next/image';
 import styles from '../../styles/Homepage.module.css';
 
 export default function Homepage() {
+    const pages = [
+        'Home',
+        'Projects',
+        'About',
+        'Contact',
+    ]
     return (
-        <div id="home">
-            hi from homepage
+        <div>
+            <div className={styles['header-container']}>
+                <div className={styles['header-content']}>
+                    {
+                        pages.map((page, index) => (
+                            <a key={index} className={styles['page-selector']} href="">{page}</a>
+                        ))
+                    }
+                </div>
+            </div>
+            <div className={styles['content-container']}>
+                <div className={styles['content-content']}>
+                    <h1 className={styles['content-title']}>Apinan Yogaratnam</h1>
+                    <h2 className={styles['content-subtitle']}>Software Engineer</h2>
+                </div>
+                <div>image goes here</div>
+            </div>
         </div>
     );
 }
